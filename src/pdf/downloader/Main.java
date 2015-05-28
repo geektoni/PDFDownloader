2795dd9ff81778e1e6ce93279f313804f23573f4
Giovanni De Toni <giovanni.det@gmail.com>
2015-05-28 22:16:32 +0200

    Added Layout inside main class. The application work (it seems).

--- src/pdf/downloader/Main.java
+++ src/pdf/downloader/Main.java
@@ -14,24 +14,15 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
     
+    final static public String TITLE = "PDFDownloader";
+    
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        Layout root = new Layout();    
+        Scene scene = new Scene(root.getLayout());
         
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle(Main.TITLE);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
