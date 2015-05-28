4b5b6c70f08fb2659294f5c1f8c99498d77daa53
Giovanni De Toni <giovanni.det@gmail.com>
2015-05-28 22:13:25 +0200

    Added a bare implementation of Layout class.

--- src/pdf/downloader/Layout.java
+++ src/pdf/downloader/Layout.java
@@ -6,6 +6,13 @@
  */
 package pdf.downloader;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
@@ -13,5 +20,50 @@ import javafx.stage.Stage;
  * @author Giovanni De Toni 
  */
 public class Layout extends Stage {
+    
+    final static private int SPACING = 20;
+    final static private Insets PADDING = new Insets(SPACING);
+    
+    private BorderPane layout;
+    private HBox controls;
+    private Button download;
+    private TextField site;
+
+    public Layout() {
+        setButtons();
+        setTextField();
+        setLayout();
+    }
+    
+    
+    public BorderPane getLayout() {
+        return layout;
+    }
+    
+    private void setLayout() {
+        layout = new BorderPane();
+        controls = new HBox();
+        
+        controls.getChildren().addAll(site, download);
+        setBox(controls);
+        
+        layout.setTop(controls);
+        
+    }
+    
+    private void setTextField() {
+        site = new TextField();
+    }
+    
+    private void setButtons() {
+        download = new Button("Download");
+    }
+    
+    private void setBox(HBox h) {
+        h.setAlignment(Pos.CENTER);
+        h.setSpacing(SPACING);
+        h.setPadding(PADDING);
+    }
+    
     
 }
