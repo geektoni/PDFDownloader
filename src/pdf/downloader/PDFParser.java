b4ed326a0db5a17e1e5ef2ac640fbb180e5b4ff4
Giovanni De Toni <giovanni.det@gmail.com>
2015-06-03 23:11:59 +0200

    Added author informations inside main and PDFParser.

--- src/pdf/downloader/PDFParser.java
+++ src/pdf/downloader/PDFParser.java
@@ -1,3 +1,9 @@
+/*
+ * This work is licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
+ *  Author: Giovanni De Toni (giovanni at detoni dot me)
+ *  Date: 25/05/2015
+ *  WebSite: www.detoni.me
+ */
 package pdf.downloader;
 
 import java.io.File;
